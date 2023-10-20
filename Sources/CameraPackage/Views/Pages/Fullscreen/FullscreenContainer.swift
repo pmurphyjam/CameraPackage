eb187da1c5ece9a423c056787a025309a06c3557
Pat Murphy <pmurphyjam@gmail.com>
2023-10-20 12:18:03 -0700

    Removing FullscreenContianer : Button Select dismiss()

--- Sources/CameraPackage/Views/Pages/Fullscreen/FullscreenContainer.swift
+++ Sources/CameraPackage/Views/Pages/Fullscreen/FullscreenContainer.swift
@@ -51,7 +51,7 @@ struct FullscreenContainer: View {
                 if selectionParamsHolder.selectionLimit == 1 {
                     Button("Select") {
                         selectionService.onSelect(assetMediaModel: selectedMediaModel)
-                        shouldDismiss()
+                        //shouldDismiss()
                     }
                     .padding([.horizontal, .bottom], 20)
                 } else {
